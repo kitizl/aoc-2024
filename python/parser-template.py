628a7f26d9f67e6ce37a6445ef612d651d6f73e6
Nithesh Balasubramanian <nithesh.dragoon98@gmail.com>
2024-12-05 08:41:16 +0100

    added some useful functions that i accidentally made for day 4

--- python/parser-template.py
+++ python/parser-template.py
@@ -16,7 +16,7 @@ class ParserTemplate:
     def read_from_file(self, filename):
         raw_data = ""
         with open(filename, "r") as f:
-            raw_data = f.buffer
+            raw_data = f.read()
         return raw_data
     
     def parse_dataset(self):
@@ -25,3 +25,39 @@ class ParserTemplate:
     def __repr__(self):
         # modify this to suit your testing needs
         return f"ParserTemplate({self.dataset!r})"
+
+
+"""
+    Other utilities I found accidentally useful.
+"""
+
+
+# for operations on two 2D lists
+
+
+def generate_padding(nrows, ncols, pad="*"): return [list(pad*ncols) for _ in range(nrows)]
+
+def padding(raw_data : str):
+    n = raw_data.find("\n")
+
+    corner = generate_padding(3, 3)
+    hor_slab = generate_padding(3, n)
+    ver_slab = generate_padding(n, 3)
+    unpadded_list2d =[ list(x) for x in  raw_data.split("\n")]
+    pure_padding = hstack(hstack(corner, hor_slab), corner)
+    data_padding = hstack(hstack(ver_slab, unpadded_list2d), ver_slab)
+    padded_list2d = vstack(vstack(pure_padding, data_padding), pure_padding)
+    return padded_list2d
+
+
+def hstack(x1, x2):
+    # check if this is compatible, but i don't care at the moment
+    # just check if the rows(x1) == rows(x2)
+
+    # in principle this function can be generalized to an
+    # arbitrary number of arguments, but i'm gonna keep it simple
+    return [[*a, *b] for (a, b) in zip(x1, x2)]
+ 
+
+def vstack(x1, x2):
+    return x1 + x2
